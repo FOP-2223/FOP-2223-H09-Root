ff4268faa9e77865bf22fc85ba4d7f821ad479fc
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-14 17:19:34 +0100

    Add listSupplier to ToListCollector

--- src/main/java/h09/sequence/ToListCollector.java
+++ src/main/java/h09/sequence/ToListCollector.java
@@ -3,11 +3,23 @@ package h09.sequence;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class ToListCollector<T> implements SequenceCollector<T, List<T>> {
+
+    private final Supplier<List<T>> listSupplier;
+
+    public ToListCollector(Supplier<List<T>> listSupplier) {
+        this.listSupplier = listSupplier;
+    }
+
+    public ToListCollector() {
+        this(ArrayList::new);
+    }
+
     @Override
     public List<T> collect(Sequence<T> sequence) {
-        final List<T> result = new ArrayList<>();
+        final List<T> result = listSupplier.get();
         final Iterator<T> iterator = sequence.iterator();
         while (iterator.hasNext()) {
             result.add(iterator.next());
