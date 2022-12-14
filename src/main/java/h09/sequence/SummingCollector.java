91af94e267d33fb35b79bc1576cef8e6e5b86f63
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-14 01:22:13 +0100

    Student template for H5

--- src/main/java/h09/sequence/SummingCollector.java
+++ src/main/java/h09/sequence/SummingCollector.java
@@ -1,26 +1,5 @@
 package h09.sequence;
 
-import h09.basic.BasicBinaryOperations;
-
-import java.util.Iterator;
-
-public class SummingCollector<T> implements SequenceCollector<T, T> {
-
-    private final T identity;
-    private final BasicBinaryOperations<T, T> operations;
-
-    public SummingCollector(T identity, BasicBinaryOperations<T, T> operations) {
-        this.identity = identity;
-        this.operations = operations;
-    }
-
-    @Override
-    public T collect(Sequence<T> sequence) {
-        T result = identity;
-        final Iterator<T> iterator = sequence.iterator();
-        while (iterator.hasNext()) {
-            result = operations.add(result, iterator.next());
-        }
-        return result;
-    }
+public class SummingCollector {
+    // TODO: H5.1 - remove if implemented
 }
--- src/main/java/h09/sequence/ToListCollector.java
+++ src/main/java/h09/sequence/ToListCollector.java
@@ -1,17 +1,5 @@
 package h09.sequence;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-public class ToListCollector<T> implements SequenceCollector<T, List<T>> {
-    @Override
-    public List<T> collect(Sequence<T> sequence) {
-        final List<T> result = new ArrayList<>();
-        final Iterator<T> iterator = sequence.iterator();
-        while (iterator.hasNext()) {
-            result.add(iterator.next());
-        }
-        return result;
-    }
+public class ToListCollector {
+    // TODO: H5.2 - remove if implemented
 }
