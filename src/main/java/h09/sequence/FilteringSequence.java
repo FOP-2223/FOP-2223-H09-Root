544955ece7eebcff7eb661c24234cab3812632fe
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-14 01:20:35 +0100

    Student template for H4

--- src/main/java/h09/sequence/FilteringSequence.java
+++ src/main/java/h09/sequence/FilteringSequence.java
@@ -1,45 +1,5 @@
 package h09.sequence;
 
-import java.util.Iterator;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
-public class FilteringSequence<T> implements Sequence<T> {
-
-    private final Sequence<T> sequence;
-    private final Predicate<? super T> predicate;
-
-    public static <T> Function<Sequence<T>, Sequence<T>> apply(Predicate<? super T> predicate) {
-        return sequence -> new FilteringSequence<>(sequence, predicate);
-    }
-
-    public FilteringSequence(Sequence<T> sequence, Predicate<? super T> predicate) {
-        this.sequence = sequence;
-        this.predicate = predicate;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<>() {
-            private final Iterator<T> iterator = sequence.iterator();
-
-            private T next;
-
-            @Override
-            public boolean hasNext() {
-                while (iterator.hasNext()) {
-                    next = iterator.next();
-                    if (predicate.test(next)) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-            @Override
-            public T next() {
-                return next;
-            }
-        };
-    }
+public class FilteringSequence {
+    // TODO: H4.2 - remove if implemented
 }
--- src/main/java/h09/sequence/TransformingSequence.java
+++ src/main/java/h09/sequence/TransformingSequence.java
@@ -1,36 +1,5 @@
 package h09.sequence;
 
-import java.util.Iterator;
-import java.util.function.Function;
-
-public class TransformingSequence<T, R> implements Sequence<R> {
-
-    private final Sequence<T> sequence;
-    private final Function<? super T, ? extends R> function;
-
-    public static <T, R> Function<Sequence<T>, Sequence<R>> apply(Function<? super T, ? extends R> function) {
-        return sequence -> new TransformingSequence<>(sequence, function);
-    }
-
-    public TransformingSequence(Sequence<T> sequence, Function<? super T, ? extends R> function) {
-        this.sequence = sequence;
-        this.function = function;
-    }
-
-    @Override
-    public Iterator<R> iterator() {
-        return new Iterator<>() {
-            private final Iterator<T> iterator = sequence.iterator();
-
-            @Override
-            public boolean hasNext() {
-                return iterator.hasNext();
-            }
-
-            @Override
-            public R next() {
-                return function.apply(iterator.next());
-            }
-        };
-    }
+public class TransformingSequence {
+    // TODO: H4.1 - remove if implemented
 }
