6900ad26e53028a8d7e3c739075608dab5332715
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-17 12:14:51 +0100

    Finish replacing primitives in DoubleWithWithCOefficientsOperator

--- src/main/java/h09/operator/DoubleSumWithCoefficientsOperator.java
+++ src/main/java/h09/operator/DoubleSumWithCoefficientsOperator.java
@@ -4,10 +4,10 @@ import java.util.function.BinaryOperator;
 
 public class DoubleSumWithCoefficientsOperator implements BinaryOperator<Double> {
 
-    private final double coeff_1;
-    private final double coeff_2;
+    private final Double coeff_1;
+    private final Double coeff_2;
 
-    public DoubleSumWithCoefficientsOperator(double coeff_1, double coeff_2) {
+    public DoubleSumWithCoefficientsOperator(Double coeff_1, Double coeff_2) {
         this.coeff_1 = coeff_1;
         this.coeff_2 = coeff_2;
     }
