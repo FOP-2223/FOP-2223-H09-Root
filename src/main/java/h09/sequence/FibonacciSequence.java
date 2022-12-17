94e35322f26dc6a44ed1e35aa1ee5919e8acc3a0
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-17 16:32:29 +0100

    Fix Fib seq

--- src/main/java/h09/sequence/FibonacciSequence.java
+++ src/main/java/h09/sequence/FibonacciSequence.java
@@ -1,5 +1,10 @@
 package h09.sequence;
 
 public class FibonacciSequence/*TODO: H3.3*/ {
-    // TODO: H3.3 - remove if implemented
+// TODO: H3.3 - uncomment if implemented
+
+//    @Override
+//    public Iterator<T> iterator() {
+//        return new FibonacciSequenceIterator();
+//    }
 }
