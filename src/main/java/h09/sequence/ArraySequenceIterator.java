2623329e3f40ed09364a3f5418b791064422eab9
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-17 01:09:43 +0100

    Fix ArraySequenceIterator

--- src/main/java/h09/sequence/ArraySequenceIterator.java
+++ src/main/java/h09/sequence/ArraySequenceIterator.java
@@ -3,20 +3,19 @@ package h09.sequence;
 import java.util.Iterator;
 
 class ArraySequenceIterator<T> implements Iterator<T> {
-    private final T[] values;
-    private int index = 0;
-
     public ArraySequenceIterator(T[] values) {
-        this.values = values;
+        // TODO: H3.1 - remove if implemented
     }
 
     @Override
     public boolean hasNext() {
-        return index < values.length;
+        // TODO: H3.1 - remove if implemented
+        throw new RuntimeException("Remove if implemented");
     }
 
     @Override
     public T next() {
-        return values[index++];
+        // TODO: H3.1 - remove if implemented
+        throw new RuntimeException("Remove if implemented");
     }
 }
