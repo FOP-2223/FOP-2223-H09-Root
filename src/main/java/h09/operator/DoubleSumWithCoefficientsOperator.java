404318cddaab8042cac8d11755a25e66c82559d4
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-18 20:33:46 +0100

    Fix some names

--- src/main/java/h09/operator/DoubleSumWithCoefficientsOperator.java
+++ src/main/java/h09/operator/DoubleSumWithCoefficientsOperator.java
@@ -4,16 +4,16 @@ import java.util.function.BinaryOperator;
 
 public class DoubleSumWithCoefficientsOperator implements BinaryOperator<Double> {
 
-    private final Double coeff_1;
-    private final Double coeff_2;
+    private final Double coeff1;
+    private final Double coeff2;
 
-    public DoubleSumWithCoefficientsOperator(Double coeff_1, Double coeff_2) {
-        this.coeff_1 = coeff_1;
-        this.coeff_2 = coeff_2;
+    public DoubleSumWithCoefficientsOperator(Double coeff1, Double coeff2) {
+        this.coeff1 = coeff1;
+        this.coeff2 = coeff2;
     }
 
     @Override
     public Double apply(Double aDouble, Double aDouble2) {
-        return coeff_1 * aDouble + coeff_2 * aDouble2;
+        return aDouble * coeff1 + aDouble2 * coeff2;
     }
 }
