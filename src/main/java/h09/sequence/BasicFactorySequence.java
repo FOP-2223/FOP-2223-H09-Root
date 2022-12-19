25676ffc59be3abaea58da709a089a74915a00cc
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-19 04:31:00 +0100

    Fix H3.4 -> H3.3

--- src/main/java/h09/sequence/BasicFactorySequence.java
+++ src/main/java/h09/sequence/BasicFactorySequence.java
@@ -4,19 +4,19 @@ import java.util.Iterator;
 
 import static org.tudalgo.algoutils.student.Student.crash;
 
-public class BasicFactorySequence/*TODO: H3.4*/ {
-// TODO: H3.4 - uncomment if implemented
+public class BasicFactorySequence/*TODO: H3.3*/ {
+// TODO: H3.3 - uncomment if implemented
 //    @Override
-    public Iterator</*TODO: H3.4 replace*/Object> iterator() {
+    public Iterator</*TODO: H3.3 replace*/Object> iterator() {
         return new Iterator<>() {
             @Override
             public boolean hasNext() {
-                return crash(); // TODO: H3.4 - remove if implemented
+                return crash(); // TODO: H3.3 - remove if implemented
             }
 
             @Override
             public /*TODO: H3.3 replace*/Object next() {
-                return crash(); // TODO: H3.4 - remove if implemented
+                return crash(); // TODO: H3.3 - remove if implemented
             }
         };
     }
