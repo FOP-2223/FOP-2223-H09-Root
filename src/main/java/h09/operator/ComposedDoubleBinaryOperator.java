55a577e66d8668191957f2943ef0f50a79c369b3
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-19 13:35:50 +0100

    Fix todo

--- src/main/java/h09/operator/ComposedDoubleBinaryOperator.java
+++ src/main/java/h09/operator/ComposedDoubleBinaryOperator.java
@@ -1,5 +1,5 @@
 package h09.operator;
 
-public class ComposedDoubleBinaryOperator/*TODO: H2.2*/ {
+public class ComposedDoubleBinaryOperator/*TODO: H2.1*/ {
     // TODO: H2.1 - remove if implemented
 }
