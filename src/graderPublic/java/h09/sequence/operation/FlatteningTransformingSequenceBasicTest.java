46a5e5888def6c826f163a1a9f738a460f55598f
Alexander Städing <alexanderstaeding@gmail.com>
2023-01-31 13:56:45 +0100

    Fixes from student public grader

--- src/graderPublic/java/h09/sequence/operation/FlatteningTransformingSequenceBasicTest.java
+++ src/graderPublic/java/h09/sequence/operation/FlatteningTransformingSequenceBasicTest.java
@@ -1,5 +1,6 @@
 package h09.sequence.operation;
 
+import h09.InvokeAssertions;
 import h09.SignatureTestExtensions;
 import h09.sequence.PrimitiveSequence;
 import h09.sequence.Sequence;
@@ -41,17 +42,17 @@ public final class FlatteningTransformingSequenceBasicTest {
                 TransformingSequence.class.getDeclaredConstructor(Sequence.class, Function.class),
             "TransformingSequence does not have a correct constructor");
         final Sequence<String> ogSeq = Sequence.of("1", "23", "456");
-        final FlatteningTransformingSequence charSeq = Assertions.assertDoesNotThrow(() ->
+        final FlatteningTransformingSequence charSeq = InvokeAssertions.assertDoesNotThrow(() ->
                 constructorF.newInstance(ogSeq, (Function<String, Sequence<Character>>) s -> PrimitiveSequence.of(s.toCharArray())),
             "FlatteningTransformingSequence constructor should not throw an exception");
-        final TransformingSequence seq = Assertions.assertDoesNotThrow(() ->
+        final TransformingSequence seq = InvokeAssertions.assertDoesNotThrow(() ->
                 constructorT.newInstance(charSeq, (Function<Character, Integer>) Character::getNumericValue),
             "TransformingSequence constructor should not throw an exception");
         Sequence<Integer> limitedSeq = new LimitSequence<>((Sequence<Integer>) seq, 6);
         Iterator<Integer> it = limitedSeq.iterator();
         for (int i = 0; i < 6; i++) {
-            Assertions.assertTrue(it.hasNext(), "FlatteningTransformingSequence should have 6 elements");
-            Assertions.assertEquals(i + 1, it.next(), "FlatteningTransformingSequence should have elements 1, 2, 3, 4, 5, 6");
+            Assertions.assertTrue(it.hasNext(), "Resulting sequence should have 6 elements! Has: " + i + " element" + (i == 1 ? "" : "s"));
+            Assertions.assertEquals(i + 1, it.next(), "Resulting sequence should be: {1, 2, 3, 4, 5, 6}! Wrong at index: " + i);
         }
     }
 }
